66cd8534c170d1d8a882434f09bc911af0784763
addis <betsega23@gmail.com>
2022-07-28 23:15:02 +0300

    Adding speakers dynamically

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const barMenu = document.querySelector('.bar-menu');
 const mainContainer = document.querySelector('.main-container');
 const menuNav = document.querySelector('.menu-nav');
 const menuList = document.querySelector('.menu-list');
+const header = document.querySelector('.header-links');
 function myFunction() {
   hamburger1.classList.toggle('active');
   barMenu.classList.toggle('active');
   mainContainer.classList.toggle('active');
-  menuNav.classList.toggle('active');
+  menuList.classList.toggle('active');
+  header.classList.toggle('active');
 }
 
 hamburger1.addEventListener('click', myFunction);
@@ -18,7 +20,8 @@ function closeBar() {
   hamburger1.classList.remove('active');
   barMenu.classList.remove('active');
   mainContainer.classList.remove('active');
-  menuNav.classList.remove('active');
+  menuList.classList.remove('active');
+  header.classList.remove('active');
 }
 navLink.forEach((n) => n.addEventListener('click', closeBar));
 
@@ -119,5 +122,4 @@ instructors.forEach((instructor) => {
   instructorProfile.appendChild(instructorInfo);
 
   loadInstructor.appendChild(card);
-  instructorSection.appendChild(loadInstructor);
 });
